793d3ff9987bec9ce58bb81dddca2dcf97fa4648
sarahsense <sts177@miami.edu>
2025-04-08 13:00:38 -0400

    Update google-doc-url.js
    edit google sheet

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1W6uMx11D1PbobUaxRv8lmXKgnDiEODNiX1XbDayy8Vg/edit?gid=0#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1W6uMx11D1PbobUaxRv8lmXKgnDiEODNiX1XbDayy8Vg/edit?gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
